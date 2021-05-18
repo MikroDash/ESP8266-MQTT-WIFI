f7d3900de8be0a78435807a69c45aba31a5b3321
Diego Careaga <al221411610@gmail.com>
2021-05-18 12:41:54 -0500

    Se actualizo la descripción

--- ARDUINO_MQTT.ino
+++ ARDUINO_MQTT.ino
@@ -14,14 +14,14 @@
 
 // Entradas y salidas Digitales
 const int button = 5; //Entrada digital
-const int ledAzul = 0;    //Salida digital
+const int ledAzul = 16;    //Salida digital
 //Variables para convertir los datos de entrada digital.
 bool statusBtn = false;
 bool candadoButton = false; 
 
 // Entradas y salidas Analogica
 const int asensorPin = A0; //Entrada analogica. Voltaje de entre 0 - 3.2 V
-const int pwmPin = 4;       //Salida analogica por PWM 
+const int pwmPin = 2;       //Salida analogica por PWM 
 //Variables para convertir los datos de entrada analogica.
 int sensorValue = 0;
 int percentValue = 0;
@@ -29,18 +29,21 @@ int lastPercentValue = 0;
 //Variables para convertir los datos de salida analogica.
 int pwmValue = 0;
 
+
 //Topics MQTT
-const char* topic_sensor = "60856dec6a7f3f9a31ff73a6/sensor";
-const char* topic_button = "60856dec6a7f3f9a31ff73a6/button";
-const char* topic_pwm = "60856dec6a7f3f9a31ff73a6/pwm";
-const char* topic_led = "60856dec6a7f3f9a31ff73a6/led";
+const char* topic_sensor = "160856dec6a7f3f9a31ff73a6/sensor";
+const char* topic_button = "160856dec6a7f3f9a31ff73a6/button";
+const char* topic_pwm = "160856dec6a7f3f9a31ff73a6/pwm";
+const char* topic_led = "160856dec6a7f3f9a31ff73a6/led";
 
 // Constantes de configuracion
-const char* ssid = "PC-MIKRODASH";              //Nombre Red Wi-Fi
+const char* ssid = "WIFI-MIKRODASH";              //Nombre Red Wi-Fi
 const char* password = "contra12345";           //Contrasenia Red Wi-Fi
 const char* mqtt_server = "mqtt.mikrodash.com"; //Servidor MQTT
 const int mqtt_port = 1883;                     //Puerto MQTT
+const int qos_level=1;                          //Calidad del servicio MQTT
 
+//Variables para la conexión y envio de mensajes MQTT
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
@@ -80,14 +83,15 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     myString = myString + (char)payload[i]; //Se convierte el payload de tipo bytes a una cadena de texto (String)
   }
-  
   JSONVar myObject = JSON.parse(myString); // Se convierte la respuesta a tipo Object para acceder a los valores por el nombre.
   if (JSON.typeof(myObject) == "undefined") { // Si el archivo no tiene información o no es JSON se sale del callback
     Serial.println("[JSON] JSON NO VALIDO"); 
     return;
   }else{                                      //Si el archivo es JSON y contiene info entra aqui
     const char* from = myObject["from"];
-    if(strcmp(topic,topic_led)==0 && strcmp(from,"app")==0){  //Si el topic es topic_led y lo envia app.mikrodash.com
+    Serial.println(strcmp(from,"app"));
+    Serial.println(myString);
+    if(strcmp(topic,topic_led)==0 && strcmp(from,"app")==0 ){  //Si el topic es topic_led y lo envia app.mikrodash.com
       if((bool) myObject["value"]){  //Si value es true       
        digitalWrite(ledAzul, HIGH);  //Enciende el LED
        client.publish(topic_led, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\": true}", true); //Actualiza el estado del LED en el topic y lo retiene
@@ -121,8 +125,8 @@ void reconnect() {
       Serial.println("Conectado");
 
       //Topics a los que se suscribira para recibir cambios
-      client.subscribe(topic_pwm);
-      client.subscribe(topic_led);
+      client.subscribe(topic_pwm,qos_level);
+      client.subscribe(topic_led,qos_level);
     } else {
       Serial.print("Error al conectar: ");
       Serial.print(client.state());
@@ -166,5 +170,5 @@ void loop() {
     snprintf (msg, MSG_BUFFER_SIZE, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\":%ld}", lastPercentValue); //Se convierte a array bits y se concatena el porcentaje del sensor
     client.publish(topic_sensor, msg, true); //Se actualiza el nuevo estado en el topic
   }
-  delay(35); //Un delay de 50ms para darle un respiro al modulo de wifi ESP8266
+  delay(45); //Un delay de 50ms para darle un respiro al modulo de wifi ESP8266
 }
