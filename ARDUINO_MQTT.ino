c6e195a35c5b727f68b83876ae22ca22b4646542
Diego Careaga <al221411610@gmail.com>
2024-03-02 19:52:51 -0600

    Update ARDUINO_MQTT.ino

--- ARDUINO_MQTT.ino
+++ ARDUINO_MQTT.ino
@@ -7,60 +7,54 @@
  y la plataforma de https://app.mikrodash.com
  para crear una dashboard personalizada
 */
+#include <PubSubClient.h> // Biblioteca para el cliente MQTT
 
-#include <ESP8266WiFi.h>  //https://github.com/esp8266/Arduino
-#include <PubSubClient.h> //https://github.com/knolleary/pubsubclient
-#include <Arduino_JSON.h> //https://github.com/arduino-libraries/Arduino_JSON
+// Incluye las bibliotecas WiFi para ESP8266 o ESP32
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+#endif
 
-// Entradas y salidas Digitales
-const int button = 5; //Entrada digital
-const int ledAzul = 16;    //Salida digital
-//Variables para convertir los datos de entrada digital.
+// Definición de pines para ESP8266 y ESP32
+#if defined(ESP8266)
+  const int buttonPin = 5; // Un pin de entrada digital típico en ESP8266
+  const int ledPin = 16; // Un pin de salida digital típico en ESP8266
+  const int sensorPin = A0; // Pin analógico en ESP8266
+  const int pwmPin = 2; // Un pin de PWM en ESP8266
+#elif defined(ESP32)
+  const int buttonPin = 5; // Un pin de entrada digital típico en ESP32
+  const int ledPin = 22; // Un pin de salida digital típico en ESP32
+  const int sensorPin = 34; // Pin analógico en ESP32 (sólo lectura)
+  const int pwmPin = 4; // Un pin de PWM en ESP32
+#endif
+
+// Variables para el estado del botón y el valor del sensor
 bool statusBtn = false;
-bool candadoButton = false; 
-
-// Entradas y salidas Analogica
-const int asensorPin = A0; //Entrada analogica. Voltaje de entre 0 - 3.2 V
-const int pwmPin = 2;       //Salida analogica por PWM 
-//Variables para convertir los datos de entrada analogica.
+bool prevButtonState = false; 
 int sensorValue = 0;
 int percentValue = 0;
 int lastPercentValue = 0;
-//Variables para convertir los datos de salida analogica.
-int pwmValue = 0;
 
+// Tópicos MQTT (debes reemplazar "your_mikrodash_id" con tu ID real de MikroDash)
+const char* topic_sensor = "your_mikrodash_id/sensor/value";
+const char* topic_button = "your_mikrodash_id/button/value";
+const char* topic_pwm = "your_mikrodash_id/pwm/percent";
+const char* topic_led = "your_mikrodash_id/led/value";
 
-//Topics MQTT
-const char* topic_sensor = "160856dec6a7f3f9a31ff73a6/sensor";
-const char* topic_button = "160856dec6a7f3f9a31ff73a6/button";
-const char* topic_pwm = "160856dec6a7f3f9a31ff73a6/pwm";
-const char* topic_led = "160856dec6a7f3f9a31ff73a6/led";
+// Configuración de la red WiFi y MQTT
+const char* ssid = "WIFI-MIKRODASH";  // Nombre de tu red WiFi
+const char* password = "contra12345"; // Contraseña de tu red WiFi
+const char* mqtt_server = "mqtt.mikrodash.com"; // Servidor MQTT
+const int mqtt_port = 1883;           // Puerto MQTT
+const char* mqtt_user = "tu_usuario_mqtt"; // Usuario para autenticación MQTT
+const char* mqtt_password = "tu_contraseña_mqtt"; // Contraseña para autenticación MQTT
 
-// Constantes de configuracion
-const char* ssid = "WIFI-MIKRODASH";              //Nombre Red Wi-Fi
-const char* password = "contra12345";           //Contrasenia Red Wi-Fi
-const char* mqtt_server = "mqtt.mikrodash.com"; //Servidor MQTT
-const int mqtt_port = 1883;                     //Puerto MQTT
-const int qos_level=1;                          //Calidad del servicio MQTT
-
-//Variables para la conexión y envio de mensajes MQTT
 WiFiClient espClient;
 PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE  (70)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
 
-
-//Conexion Wi-Fi
 void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Conectando a ");
-  Serial.println(ssid);
-
+  Serial.println("Conectando a la red WiFi...");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -69,106 +63,81 @@ void setup_wifi() {
     Serial.print(".");
   }
 
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi Conectado");
-  Serial.println("Direccion IP: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("\nWiFi conectado");
+  Serial.println("Dirección IP: " + WiFi.localIP().toString());
 }
 
-//Callback para la recepcion de datos en los topic suscrito
 void callback(char* topic, byte* payload, unsigned int length) {
-  String myString = ""; 
+  String incomingMessage = "";
   for (int i = 0; i < length; i++) {
-    myString = myString + (char)payload[i]; //Se convierte el payload de tipo bytes a una cadena de texto (String)
-  }
-  JSONVar myObject = JSON.parse(myString); // Se convierte la respuesta a tipo Object para acceder a los valores por el nombre.
-  if (JSON.typeof(myObject) == "undefined") { // Si el archivo no tiene información o no es JSON se sale del callback
-    Serial.println("[JSON] JSON NO VALIDO"); 
-    return;
-  }else{                                      //Si el archivo es JSON y contiene info entra aqui
-    const char* from = myObject["from"];
-    Serial.println(strcmp(from,"app"));
-    Serial.println(myString);
-    if(strcmp(topic,topic_led)==0 && strcmp(from,"app")==0 ){  //Si el topic es topic_led y lo envia app.mikrodash.com
-      if((bool) myObject["value"]){  //Si value es true       
-       digitalWrite(ledAzul, HIGH);  //Enciende el LED
-       client.publish(topic_led, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\": true}", true); //Actualiza el estado del LED en el topic y lo retiene
-      }else{
-        digitalWrite(ledAzul, LOW); //Apaga el led
-        client.publish(topic_led, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\": false}", true); //Actualiza el estado del LED en el topic y lo retiene
-      }
-    }else if(strcmp(topic,topic_pwm)==0 && strcmp(from,"app")==0){ //Si el topic es topic_pwm y lo envia app.mikrodash.com
-      const int pwmValue = (int) myObject["value"];   //Se asigna el valor de "Value" en una constante de tipo entero
-      if(pwmValue >= 0 && pwmValue <= 100){           
-        const int setValue = (pwmValue * 1024) / 100; //se convierte de un porcentaje a un valor asignable al PWM con una simple regla de 3
-        snprintf (msg, MSG_BUFFER_SIZE, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\":%ld}", pwmValue);  //convierte los parametros en un array de bytes
-        client.publish(topic_pwm, msg, true); //Se actualiza el valor actual de la salida PWM
-        analogWrite(pwmPin, setValue); 
-      }
-    }
+    incomingMessage += (char)payload[i];
   }
 
+  if (strcmp(topic, topic_led) == 0) {
+    digitalWrite(ledPin, incomingMessage.toInt() ? HIGH : LOW);
+  } else if (strcmp(topic, topic_pwm) == 0) {
+    int pwmValue = map(incomingMessage.toInt(), 0, 100, 0, 255);
+    #if defined(ESP8266)
+      analogWrite(pwmPin, pwmValue);
+    #elif defined(ESP32)
+      ledcWrite(0, pwmValue); // Usando canal 0 para ESP32 PWM
+    #endif
+  }
 }
 
-// Funcion para reconectar
 void reconnect() {
   while (!client.connected()) {
-    Serial.print("Conectando al broker MQTT...");
-    // Se genera un ID aleatorio con
-    String clientId = "MikroDashWiFiClient-";
-    clientId += String(random(0xffff), HEX);
-
-    //Cuando se conecta
-    if (client.connect(clientId.c_str())) {
+    Serial.print("Intentando conexión MQTT...");
+    if (client.connect("ESPClient", mqtt_user, mqtt_password)) {
       Serial.println("Conectado");
-
-      //Topics a los que se suscribira para recibir cambios
-      client.subscribe(topic_pwm,qos_level);
-      client.subscribe(topic_led,qos_level);
+      client.subscribe(topic_led);
+      client.subscribe(topic_pwm);
     } else {
-      Serial.print("Error al conectar: ");
+      Serial.print("falló, rc=");
       Serial.print(client.state());
-      Serial.println(" Reintentando en 5 segundos...");
+      Serial.println(" intentando de nuevo en 5 segundos");
       delay(5000);
     }
   }
 }
 
-//Funcion principal para configura el modulo
 void setup() {
-  pinMode(ledAzul, OUTPUT);  //Se declara el pin como salida
-  Serial.begin(115200);      //Comunicación serial para monitorear
-  setup_wifi();               //Se llama la funcición para conectarse al Wi-Fi
-  client.setServer(mqtt_server, mqtt_port);  //Se indica el servidor y puerto de MQTT para conectarse
-  client.setCallback(callback);  //Se establece el callback, donde se reciben los cambios de las suscripciones MQTT
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  
+  #if defined(ESP32)
+    // Configuración PWM para ESP32
+    ledcAttachPin(pwmPin, 0); // Asigna el pin PWM al canal 0
+    ledcSetup(0, 5000, 8); // Configura el canal 0, 5000 Hz, 8 bits
+  #endif
 }
 
-//Bucle infinito
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-  if (!client.connected()) { //Si se detecta que no hay conexión con el broker 
-    reconnect();              //Reintenta la conexión
+  // Leer estado del botón
+  bool currentStatus = digitalRead(buttonPin);
+  if (prevButtonState != currentStatus) {
+    prevButtonState = currentStatus;
+    client.publish(topic_button, currentStatus ? "1" : "0", true);
   }
-  client.loop();            //De lo contrario crea un bucle para las suscripciones MQTT
 
-  statusBtn = digitalRead(button); //Se lee el estado del boton o entrada digital
-  if(candadoButton != statusBtn){  //Condiciones para entrar solo en cada cambio de estado del boton
-    candadoButton = statusBtn;
-    if(candadoButton){
-      client.publish(topic_button, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\": true}", true); //Se actualiza el nuevo estado en el topic
-    }else{
-      client.publish(topic_button, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\": false}", true); //Se actualiza el nuevo estado en el topic
-    }
+  // Leer y publicar valor del sensor
+  sensorValue = analogRead(sensorPin);
+  percentValue = map(sensorValue, 0, 1023, 0, 100); // Ajustar rango si es ESP32
+  if (percentValue != lastPercentValue) {
+    lastPercentValue = percentValue;
+    String msg = String(percentValue);
+    client.publish(topic_sensor, msg.c_str(), true);
   }
-  
-  sensorValue = analogRead(asensorPin); //Se lee la entrada analógica
-  percentValue = (sensorValue * 100) / 1024;  //Se convierte de un resolucion de 0 a 1024 a un porcentaje de 0 a 100 con una regla de 3  
-  if(lastPercentValue != percentValue){  //Solo si hay un cambio en el sensor entra
-    lastPercentValue = percentValue;    
-    snprintf (msg, MSG_BUFFER_SIZE, "{\"from\":\"device\",\"message\":\"Hola desde ESP8266\",\"value\":%ld}", lastPercentValue); //Se convierte a array bits y se concatena el porcentaje del sensor
-    client.publish(topic_sensor, msg, true); //Se actualiza el nuevo estado en el topic
-  }
-  delay(45); //Un delay de 50ms para darle un respiro al modulo de wifi ESP8266
+
+  delay(100);
 }
